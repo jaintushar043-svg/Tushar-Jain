b517667dadfa626d051114ced9c50161c990281f
jaintushar043-svg <jaintushar043@gmail.com>
2026-07-30 16:15:35 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-from pydantic_settings import BaseSettings
-
-
-class Settings(BaseSettings):
-    database_url: str = "postgresql+psycopg2://aegis:aegis_dev_pw@localhost:5432/aegis"
-
-
-settings = Settings()
